0bb1d8563c2e0f085306133e36e0944e595af34a
natalia-bay <nat.bayduraeva@gmail.com>
2021-02-23 13:16:17 +0800

    Moved code to github

---
+++ src/index.js
@@ -0,0 +1,69 @@
+function formatDay(date) {
+  let dayIndex = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[dayIndex];
+}
+
+let currentTime = new Date();
+let dayElement = document.querySelector("#day1");
+dayElement.innerHTML = formatDay(currentTime);
+
+function getWeather(city) {
+  let units = `metric`;
+  let apiKey = "7345ee018fd528da4cd97bec34042c86";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  getWeather(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+getWeather("Berlin");
+
+function showWeather(response) {
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#degrees-now").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#feelsLike").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#windSpeed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#pressure").innerHTML = response.data.main.pressure;
+  document.querySelector("#tempMax").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#tempMin").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(fetchLocation);
+}
+
+function fetchLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "7345ee018fd528da4cd97bec34042c86";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+let currentLocation = document.querySelector("#current-location");
+currentLocation.addEventListener("click", getCurrentLocation);
