1d88118a2eb4b5bcc662c009977f9c102627ef98
natalia-bay <nat.bayduraeva@gmail.com>
2021-02-26 11:50:47 +0800

    Adjusted weather description and moved weather icon

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ function showWeather(response) {
     response.data.main.temp
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
   document.querySelector("#feelsLike").innerHTML = Math.round(
     response.data.main.feels_like
   );
